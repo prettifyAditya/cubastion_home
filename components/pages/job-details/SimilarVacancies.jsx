6c58f04d272719bd58c6c73b53f93c14db21252b
prettifyAditya <adityashukla.prettifycreative@gmail.com>
2026-03-27 19:39:01 +0530

    Completed responsiveness

--- components/pages/job-details/SimilarVacancies.jsx
+++ components/pages/job-details/SimilarVacancies.jsx
@@ -38,7 +38,24 @@ export default function SimilarVacancies({ data }){
                             cardType="CareerCard"
                             loop={false}
                             speed={800}
-                            slidesPerView={4}
+                            breakpoints={{
+                                0: {
+                                    slidesPerView: 1.2,
+                                    spaceBetween: 10,
+                                },
+                                540: {
+                                    slidesPerView: 2,
+                                    spaceBetween: 20,
+                                },
+                                768: {
+                                    slidesPerView: 3,
+                                    spaceBetween: 20,
+                                },
+                                991: {
+                                    slidesPerView: 4,
+                                    spaceBetween: 20,
+                                },
+                            }}
                             classname="vacancies_slider"
                             onSwiper={(swiper) => {
                                 (swiperRef.current = swiper);
